0bf8c560c295544cfbe52537108010d4aebb1df5
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-02-08 19:59:35 +0100

    Started writing the structure

---
+++ game.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+
+struct Game {
+    char matrix[50][80]; // Il campo di gioco
+    int points; // Punteggio del giocatore
+    std::string name; // Nome del giocatore
+};
---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "cleanScreen.hpp"
+
+
+int game() {
+    // A game
+    Game myGame;
+}
+
+
+int main() {
+    // Chiediamo le varie informazioni all'utente
+    // Salviamo tutti i dati in dei file .txt (di testo)
+
+    while (true) {
+        game(/* Passiamo i dati alla funzione */);
+
+        // Diciamo quanti punti ha fatto
+
+        // Chiediamo se vuole rigiocare
+    }
+}

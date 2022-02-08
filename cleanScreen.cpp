e7e8e43f874887c54292c87892dd8971b89da85b
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-02-08 17:02:13 +0100

    cleanScreen

---
+++ cleanScreen.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "cleanScreen.hpp"
+
+
+int main() {
+	std::cout << "Hello, ";
+	cleanScreen();
+	std::cout << "World!";
+}
---
+++ cleanScreen.hpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <stdlib.h>
+
+
+void cleanScreen() {
+	system("cls");
+}
